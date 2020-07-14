38994dcdc5bf3ac84db7b93e79b625589ef2c001
Hugo Dionizio Santos <dnhutos@hotmail.com>
2020-07-13 22:28:43 -0300

    Convertendo sistema

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+//var express = require('express')
+//var path = require('path')
+//var serveStatic = require('serve-static')
+
+//var app = express()
+//app.use(serveStatic(path.join(__dirname, 'dist')))
+
+//var port = process.env.PORT || 3000
+//app.listen(port)
+//console.log('server started ' + port)
+
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

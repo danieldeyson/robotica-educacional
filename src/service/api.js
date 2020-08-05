f4fa612e283e47947aeb738f1dd70babdc5e9a0f
Hugo Dionizio Santos <hugodionizio@users.noreply.github.com>
2020-08-05 01:08:22 -0300

    Trocando caminho da API
    A API do servidor local (localhost) fica comentada e o a API do servidor do Heroku fica ativo

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const api = axios.create({
   //baseURL: process.env.REACT_APP_API_URL
-  //baseURL: 'https://science-robotica-back.herokuapp.com/api/',
-	baseURL: 'http://localhost:8080/api/',
+  baseURL: 'https://science-robotica-back.herokuapp.com/api/',
+  //baseURL: 'http://localhost:8080/api/',
 })
 
 export default api
